6b838d5520e63c76b54d9ba8b5f67dd9509c3019
Matt Kubilus <mattkubilus@gmail.com>
2015-12-20 14:10:20 -0500

    Refactor tilemap.  Try to make things a bit more self-contained.

--- incs/tilemap.h
+++ incs/tilemap.h
@@ -2,20 +2,42 @@
 #define _MAP
 #include "genesis.h"
 
-u8 planwidth;
-u8 planheight;
+//u8 planwidth;
+//u8 planheight;
 
 // winX and winY -> tile location in tilemap
-u16 winX;
-u16 winY;
+//u16 winX;
+//u16 winY;
 
 // tX and tY -> tile position in the plane
-u16 tX;
-u16 tY;
+//u16 tX;
+//u16 tY;
 
 // maph and mapw -> height and width of tilemap
-u16 maph;
-u16 mapw;
+//u16 maph;
+//u16 mapw;
+
+typedef struct d_map{
+    u8 planwidth;
+    u8 planheight;
+    // winX and winY -> tile location in tilemap
+    u16 winX;
+    u16 winY;
+    // tX and tY -> tile position in the plane
+    u16 tX;
+    u16 tY;
+    // maph and mapw -> height and width of tilemap
+    u16 maph;
+    u16 mapw;
+
+    u16 tileoffset;
+} datamap;
+
+//datamap _blastdata = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+//datamap* blastdata = &_blastdata;
+extern datamap* blastdata;
+
 
 // winH and winW -> height and width of visible window in tiles
 #define winH 28
@@ -27,8 +49,19 @@ typedef struct t_map{
     u8* data;
 } tilemap;
 
-void load_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tileoffset);
-void load_visible_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tileoffset);
-void load_map_row(const tilemap* inmap, u16 plan, u8 row, u16 tileoffset);
-void load_map_col(const tilemap* inmap, u16 plan, u8 col, u16 tileoffset);
+typedef struct t_blastmap{
+    datamap* data;
+    tilemap* tiles;
+} blastmap;
+
+//void blast_init(datamap* blastdata);
+void blast_init(blastmap* bmap);
+void load_map(blastmap* bmap, int xoffset, int yoffset, u16 plan, u16 tileoffset);
+void load_visible_map(blastmap* bmap, int xoffset, int yoffset, u16 plan, u16 tileoffset);
+void load_map_row(blastmap* bmap, u16 plan, u8 row, u16 tileoffset);
+void load_map_col(blastmap* bmap, u16 plan, u8 col, u16 tileoffset);
+void screen_left(blastmap* bmap, u16* hScroll, u16 tile_offset, u16 plan);
+void screen_right(blastmap* bmap, u16* hScroll, u16 tile_offset, u16 plan);
+void screen_up(blastmap* bmap, u16* vScroll, u16 tile_offset, u16 plan);
+void screen_down(blastmap* bmap, u16* vScroll, u16 tile_offset, u16 plan);
 #endif
--- libs/tilemap.c
+++ libs/tilemap.c
@@ -1,26 +1,100 @@
+#include "genesis.h"
 #include "tilemap.h"
 
-void load_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tileoffset) {
+
+//void blast_init(datamap* blastdata) {
+void blast_init(blastmap* bmap) {
+    //blastdata = malloc(sizeof(datamap));
+    bmap->data->tX = 64; 
+    bmap->data->tY = 64;
+    bmap->data->winX = bmap->data->tX;
+    bmap->data->winY = bmap->data->tY;
+    bmap->data->maph = 0;
+    bmap->data->mapw = 0;    
+    bmap->data->planwidth = VDP_getPlanWidth();
+    bmap->data->planheight = VDP_getPlanHeight();
+}
+
+void screen_left(blastmap* bmap, u16* hScroll, u16 tile_offset, u16 plan) {
+    *hScroll+=1;
+    if(*hScroll % 8 == 0) {
+        bmap->data->tX-=1;
+        bmap->data->winX-=1;
+        if(bmap->data->tX>=bmap->data->planwidth){
+            bmap->data->tX = bmap->data->planwidth - 1;
+        }
+        if(bmap->data->winX>=bmap->data->mapw) {
+            bmap->data->winX = bmap->data->mapw - 1;
+        }
+        load_map_col(bmap,plan,0,tile_offset);
+    }
+}
+
+void screen_right(blastmap* bmap, u16* hScroll, u16 tile_offset, u16 plan) {
+    *hScroll-=1;
+    if(*hScroll % 8 == 0) {
+        bmap->data->tX+=1;
+        bmap->data->winX+=1;
+        if(bmap->data->tX>=bmap->data->planwidth){
+            bmap->data->tX = 0;
+        }
+        if(bmap->data->winX>=bmap->data->mapw) {
+            bmap->data->winX = 0;
+        }
+        load_map_col(bmap,plan,winW+1,tile_offset);
+    }
+}
+
+void screen_up(blastmap* bmap, u16* vScroll, u16 tile_offset, u16 plan) {
+    *vScroll-=1;
+    if(*vScroll % 8 == 0) {
+        bmap->data->tY-=1;
+        bmap->data->winY-=1;
+        if(bmap->data->tY>=bmap->data->planheight){
+            bmap->data->tY = bmap->data->planheight - 1;
+        }
+        if(bmap->data->winY>=bmap->data->maph){
+            bmap->data->winY = bmap->data->maph - 1;
+        }
+        load_map_row(bmap,plan,0,tile_offset);
+    }
+}
+void screen_down(blastmap* bmap, u16* vScroll, u16 tile_offset, u16 plan) {
+    *vScroll+=1;
+    if(*vScroll % 8 == 0) {
+        bmap->data->tY+=1;
+        bmap->data->winY+=1;
+        if(bmap->data->tY>=bmap->data->planheight){
+            bmap->data->tY = 0;
+        }
+        if(bmap->data->winY>=bmap->data->maph){
+            bmap->data->winY = 0;
+        }
+        load_map_row(bmap,plan,winH+1,tile_offset);
+    }
+}
+
+void load_map(blastmap* bmap, int xoffset, int yoffset, u16 plan, u16 tileoffset) {
     int i;
     int j;
 
     //int start_index = 1;
-    int maph = inmap->height;
-    int mapw = inmap->width;
+    bmap->data->maph = bmap->tiles->height;
+    bmap->data->mapw = bmap->tiles->width;
 
     int tilex;
     int tiley;
     
-    for(i=0;i<maph;i++) {
-        if(i+yoffset >= planheight) {
-            tiley = (i + yoffset) - planheight;
+    for(i=0;i<bmap->data->maph;i++) {
+        if(i+yoffset >= bmap->data->planheight) {
+            tiley = (i + yoffset) - bmap->data->planheight;
         } else {
             tiley = i + yoffset;
         }
 
-        for(j=0;j<mapw;j++) {
-            if(j+xoffset >= planwidth) {
-                tilex = (j + xoffset) - planwidth;
+        for(j=0;j<bmap->data->mapw;j++) {
+            if(j+xoffset >= bmap->data->planwidth) {
+                tilex = (j + xoffset) - bmap->data->planwidth;
 
             } else {
                 tilex = j + xoffset;
@@ -28,7 +102,7 @@ void load_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tile
             }
             VDP_setTileMapXY(
                     plan, 
-                    (u16*)inmap->data[(i * mapw) + j] + tileoffset - 1,
+                    (u16*)bmap->tiles->data[(i * (bmap->data->mapw)) + j] + tileoffset - 1,
                     tilex,
                     tiley
             );
@@ -36,7 +110,7 @@ void load_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tile
     }
 }
 
-void load_visible_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, u16 tileoffset) {
+void load_visible_map(blastmap* bmap, int xoffset, int yoffset, u16 plan, u16 tileoffset) {
     u16 i;
     u16 j;
     u16 iW;
@@ -44,33 +118,33 @@ void load_visible_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, 
     u8 ycount;
     u8 xcount;
 
-    mapw = inmap->width;
-    maph = inmap->height;
+    bmap->data->mapw = bmap->tiles->width;
+    bmap->data->maph = bmap->tiles->height;
 
-    i = tY;
-    iW = winY;
+    i = bmap->data->tY;
+    iW = bmap->data->winY;
     for(ycount=0;ycount<winH;ycount++) {
 
         // wrap around
-        if(i == planheight) {
-            i -= planheight;
+        if(i == bmap->data->planheight) {
+            i -= bmap->data->planheight;
         } 
-        if(iW >= maph) {
-            iW -= maph;
+        if(iW >= bmap->data->maph) {
+            iW -= bmap->data->maph;
         }
 
-        j = tX;
-        jW = winX;
+        j = bmap->data->tX;
+        jW = bmap->data->winX;
         for(xcount=0;xcount<winW;xcount++){
-            if(j == planwidth) {
-                j -= planwidth;
+            if(j == bmap->data->planwidth) {
+                j -= bmap->data->planwidth;
             } 
-            if(jW == mapw) {
+            if(jW == bmap->data->mapw) {
                 jW = 0;
             }
             VDP_setTileMapXY(
                     plan, 
-                    (u16*)inmap->data[(iW * mapw) + jW] + tileoffset - 1,
+                    (u16*)(bmap->tiles->data[(iW * (bmap->data->mapw)) + jW]) + tileoffset - 1,
                     j,
                     i
             );
@@ -83,36 +157,36 @@ void load_visible_map(const tilemap* inmap, int xoffset, int yoffset, u16 plan, 
 
 }
 
-void load_map_row(const tilemap* inmap, u16 plan, u8 row, u16 tileoffset) {
+void load_map_row(blastmap* bmap, u16 plan, u8 row, u16 tileoffset) {
     u8 i;
     u8 j;
     u16 iW;
     u16 jW;
     u8 xcount;
 
-    i = tY + row;
-    iW = winY + row;
+    i = bmap->data->tY + row;
+    iW = bmap->data->winY + row;
 
     // wrap around
-    if(i >= planheight) {
-        i -= planheight;
+    if(i >= bmap->data->planheight) {
+        i -= bmap->data->planheight;
     } 
-    if(iW >= maph) {
-        iW -= maph;
+    if(iW >= bmap->data->maph) {
+        iW -= bmap->data->maph;
     }
 
-    j = tX;
-    jW = winX;
+    j = bmap->data->tX;
+    jW = bmap->data->winX;
     for(xcount=0;xcount<winW;xcount++){
-        if(j == planwidth) {
+        if(j == bmap->data->planwidth) {
             j = 0;
         } 
-        if(jW == mapw) {
+        if(jW == bmap->data->mapw) {
             jW = 0;
         }
         VDP_setTileMapXY(
                 plan, 
-                (u16*)inmap->data[(iW * mapw) + jW] + tileoffset - 1,
+                (u16*)bmap->tiles->data[(iW * (bmap->data->mapw)) + jW] + tileoffset - 1,
                 j,
                 i
         );
@@ -121,36 +195,36 @@ void load_map_row(const tilemap* inmap, u16 plan, u8 row, u16 tileoffset) {
     }
 }
 
-void load_map_col(const tilemap* inmap, u16 plan, u8 col, u16 tileoffset) {
+void load_map_col(blastmap* bmap, u16 plan, u8 col, u16 tileoffset) {
     u8 i;
     u8 j;
     u16 iW;
     u16 jW;
     u8 ycount;
 
-    i = tX + col;
-    iW = winX + col;
+    i = bmap->data->tX + col;
+    iW = bmap->data->winX + col;
 
     // wrap around
-    if(i >= planwidth) {
-        i -= planwidth;
+    if(i >= bmap->data->planwidth) {
+        i -= bmap->data->planwidth;
     } 
-    if(iW >= mapw) {
-        iW -= mapw;
+    if(iW >= bmap->data->mapw) {
+        iW -= bmap->data->mapw;
     }
 
-    j = tY;
-    jW = winY;
+    j = bmap->data->tY;
+    jW = bmap->data->winY;
     for(ycount=0;ycount<winH;ycount++){
-        if(j == planheight) {
+        if(j == bmap->data->planheight) {
             j = 0;
         } 
-        if(jW == maph) {
+        if(jW == bmap->data->maph) {
             jW = 0;
         }
         VDP_setTileMapXY(
                 plan, 
-                (u16*)inmap->data[(jW * mapw) + iW] + tileoffset - 1,
+                (u16*)bmap->tiles->data[(jW * (bmap->data->mapw)) + iW] + tileoffset - 1,
                 i,
                 j
         );
